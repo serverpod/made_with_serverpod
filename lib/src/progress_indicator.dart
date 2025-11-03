e11d894dbf3ec3b64679743a90cbfb50d70e19d0
Viktor Lidholt <viktorlidholt@gmail.com>
2025-11-03 16:59:11 +0100

    feat: Adds fade-in when loading progress indicator.

--- lib/src/progress_indicator.dart
+++ lib/src/progress_indicator.dart
@@ -12,6 +12,17 @@ class ServerpodProgressIndicator extends StatelessWidget {
       height: size,
       child: Image.asset(
         'assets/nova-running.webp',
+        frameBuilder: (BuildContext context, Widget child, int? frame,
+            bool wasSynchronouslyLoaded) {
+          if (wasSynchronouslyLoaded) {
+            return child;
+          }
+          return AnimatedOpacity(
+            opacity: frame == null ? 0 : 1,
+            duration: const Duration(seconds: 1),
+            child: child,
+          );
+        },
         package: 'made_with_serverpod',
         width: size,
         height: size,
